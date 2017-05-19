97321417af8ee12df551e0283c7b828dec665c6a
Nathan Flurry <developer@nathanflurry.com>
2017-05-18 21:07:42 -0700

    Moved to ES6

--- dist/react-progressbar.js
+++ dist/react-progressbar.js
@@ -1,17 +1,21 @@
 (function (global, factory) {
     if (typeof define === "function" && define.amd) {
-        define(['module', 'react', 'react-dom', 'lodash.isequal', 'progressbar.js'], factory);
+        define(['exports', 'react', 'react-dom', 'lodash.isequal', 'progressbar.js'], factory);
     } else if (typeof exports !== "undefined") {
-        factory(module, require('react'), require('react-dom'), require('lodash.isequal'), require('progressbar.js'));
+        factory(exports, require('react'), require('react-dom'), require('lodash.isequal'), require('progressbar.js'));
     } else {
         var mod = {
             exports: {}
         };
-        factory(mod, global.react, global.reactDom, global.lodash, global.progressbar);
+        factory(mod.exports, global.react, global.reactDom, global.lodash, global.progressbar);
         global.main = mod.exports;
     }
-})(this, function (module, React, ReactDom, isEqual, ProgressBar) {
+})(this, function (exports, React, ReactDom, isEqual, ProgressBar) {
     'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
 
     var _extends = Object.assign || function (target) {
         for (var i = 1; i < arguments.length; i++) {
@@ -27,122 +31,221 @@
         return target;
     };
 
-    var Shape = React.createClass({
-        displayName: 'Shape',
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
 
-        getDefaultProps: function getDefaultProps() {
-            return {
-                ShapeClass: null,
-                options: {},
-                progress: 0,
-                text: null,
-                initialAnimate: false,
-                containerStyle: {},
-                containerClassName: '.progressbar-container'
-            };
-        },
-
-        getInitialState: function getInitialState() {
-            return {
-                shape: null
-            };
-        },
-
-        render: function render() {
-            var style = this.props.containerStyle;
-            var className = this.props.containerClassName;
-
-            return React.createElement('div', { className: className, style: style, ref: 'progressBar' });
-        },
-
-        componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-            if (!isEqual(this.props.options, nextProps.options)) {
-                this._destroy();
-                this._create(nextProps, this.props);
-                return;
+    var _createClass = function () {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
             }
+        }
 
-            this._animateProgress(nextProps.progress);
-            this._setText(nextProps.text);
-        },
+        return function (Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
 
-        componentDidMount: function componentDidMount() {
-            this._create(this.props);
-        },
+    function _possibleConstructorReturn(self, call) {
+        if (!self) {
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        }
 
-        componentWillUnmount: function componentWillUnmount() {
-            this._destroy();
-        },
+        return call && (typeof call === "object" || typeof call === "function") ? call : self;
+    }
 
-        _create: function _create(props, oldProps) {
-            if (this.state.shape !== null) {
-                throw new Error('Progressbar is already created');
+    function _inherits(subClass, superClass) {
+        if (typeof superClass !== "function" && superClass !== null) {
+            throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+        }
+
+        subClass.prototype = Object.create(superClass && superClass.prototype, {
+            constructor: {
+                value: subClass,
+                enumerable: false,
+                writable: true,
+                configurable: true
             }
+        });
+        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+    }
 
-            // setState function is not used to prevent a new render cycle
-            // This handling happens outside of React component's lifecycle
-            var container = ReactDom.findDOMNode(this.refs.progressBar);
-            this.state.shape = new props.ShapeClass(container, props.options);
+    var Shape = exports.Shape = function (_React$Component) {
+        _inherits(Shape, _React$Component);
 
-            if (props.initialAnimate) {
-                if (oldProps) {
-                    this._setProgress(oldProps.progress);
+        function Shape() {
+            _classCallCheck(this, Shape);
+
+            return _possibleConstructorReturn(this, (Shape.__proto__ || Object.getPrototypeOf(Shape)).apply(this, arguments));
+        }
+
+        _createClass(Shape, [{
+            key: 'getDefaultProps',
+            value: function getDefaultProps() {
+                return {
+                    ShapeClass: null,
+                    options: {},
+                    progress: 0,
+                    text: null,
+                    initialAnimate: false,
+                    containerStyle: {},
+                    containerClassName: '.progressbar-container'
+                };
+            }
+        }, {
+            key: 'getInitialState',
+            value: function getInitialState() {
+                return {
+                    shape: null
+                };
+            }
+        }, {
+            key: 'render',
+            value: function render() {
+                var style = this.props.containerStyle;
+                var className = this.props.containerClassName;
+
+                return React.createElement('div', { className: className, style: style, ref: 'progressBar' });
+            }
+        }, {
+            key: 'componentWillReceiveProps',
+            value: function componentWillReceiveProps(nextProps) {
+                if (!isEqual(this.props.options, nextProps.options)) {
+                    this._destroy();
+                    this._create(nextProps, this.props);
+                    return;
                 }
 
-                this._animateProgress(props.progress);
-            } else {
-                this._setProgress(props.progress);
+                this._animateProgress(nextProps.progress);
+                this._setText(nextProps.text);
             }
-
-            this._setText(props.text);
-        },
-
-        _destroy: function _destroy() {
-            if (this.state.shape) {
-                this.state.shape.destroy();
-                this.state.shape = null;
+        }, {
+            key: 'componentDidMount',
+            value: function componentDidMount() {
+                this._create(this.props);
             }
-        },
-
-        _animateProgress: function _animateProgress(progress) {
-            this.state.shape.animate(progress);
-        },
-
-        _setProgress: function _setProgress(progress) {
-            this.state.shape.set(progress);
-        },
-
-        _setText: function _setText(text) {
-            if (text) {
-                this.state.shape.setText(text);
+        }, {
+            key: 'componentWillUnmount',
+            value: function componentWillUnmount() {
+                this._destroy();
             }
-        }
-    });
+        }, {
+            key: '_create',
+            value: function _create(props, oldProps) {
+                if (this.state.shape !== null) {
+                    throw new Error('Progressbar is already created');
+                }
 
-    var Line = React.createClass({
-        displayName: 'Line',
-        render: function render() {
-            return React.createElement(Shape, _extends({}, this.props, { ShapeClass: ProgressBar.Line }));
-        }
-    });
+                // setState function is not used to prevent a new render cycle
+                // This handling happens outside of React component's lifecycle
+                var container = ReactDom.findDOMNode(this.refs.progressBar);
+                this.state.shape = new props.ShapeClass(container, props.options);
 
-    var Circle = React.createClass({
-        displayName: 'Circle',
-        render: function render() {
-            return React.createElement(Shape, _extends({}, this.props, { ShapeClass: ProgressBar.Circle }));
-        }
-    });
+                if (props.initialAnimate) {
+                    if (oldProps) {
+                        this._setProgress(oldProps.progress);
+                    }
 
-    var SemiCircle = React.createClass({
-        displayName: 'SemiCircle',
-        render: function render() {
-            return React.createElement(Shape, _extends({}, this.props, { ShapeClass: ProgressBar.SemiCircle }));
-        }
-    });
+                    this._animateProgress(props.progress);
+                } else {
+                    this._setProgress(props.progress);
+                }
 
-    module.exports = {
-        Line: Line,
-        Circle: Circle,
-        SemiCircle: SemiCircle
-    };
+                this._setText(props.text);
+            }
+        }, {
+            key: '_destroy',
+            value: function _destroy() {
+                if (this.state.shape) {
+                    this.state.shape.destroy();
+                    this.state.shape = null;
+                }
+            }
+        }, {
+            key: '_animateProgress',
+            value: function _animateProgress(progress) {
+                this.state.shape.animate(progress);
+            }
+        }, {
+            key: '_setProgress',
+            value: function _setProgress(progress) {
+                this.state.shape.set(progress);
+            }
+        }, {
+            key: '_setText',
+            value: function _setText(text) {
+                if (text) {
+                    this.state.shape.setText(text);
+                }
+            }
+        }]);
+
+        return Shape;
+    }(React.Component);
+
+    var Line = exports.Line = function (_Shape) {
+        _inherits(Line, _Shape);
+
+        function Line() {
+            _classCallCheck(this, Line);
+
+            return _possibleConstructorReturn(this, (Line.__proto__ || Object.getPrototypeOf(Line)).apply(this, arguments));
+        }
+
+        _createClass(Line, [{
+            key: 'render',
+            value: function render() {
+                return React.createElement(Shape, _extends({}, this.props, { ShapeClass: ProgressBar.Line }));
+            }
+        }]);
+
+        return Line;
+    }(Shape);
+
+    var Circle = exports.Circle = function (_Shape2) {
+        _inherits(Circle, _Shape2);
+
+        function Circle() {
+            _classCallCheck(this, Circle);
+
+            return _possibleConstructorReturn(this, (Circle.__proto__ || Object.getPrototypeOf(Circle)).apply(this, arguments));
+        }
+
+        _createClass(Circle, [{
+            key: 'render',
+            value: function render() {
+                return React.createElement(Shape, _extends({}, this.props, { ShapeClass: ProgressBar.Circle }));
+            }
+        }]);
+
+        return Circle;
+    }(Shape);
+
+    var SemiCircle = exports.SemiCircle = function (_Shape3) {
+        _inherits(SemiCircle, _Shape3);
+
+        function SemiCircle() {
+            _classCallCheck(this, SemiCircle);
+
+            return _possibleConstructorReturn(this, (SemiCircle.__proto__ || Object.getPrototypeOf(SemiCircle)).apply(this, arguments));
+        }
+
+        _createClass(SemiCircle, [{
+            key: 'render',
+            value: function render() {
+                return React.createElement(Shape, _extends({}, this.props, { ShapeClass: ProgressBar.SemiCircle }));
+            }
+        }]);
+
+        return SemiCircle;
+    }(Shape);
 });
--- local-dev/main.js
+++ local-dev/main.js
@@ -3,39 +3,41 @@ var ReactDom = require('react-dom');
 var ProgressBar = require('../src/main.js');
 var Circle = ProgressBar.Circle;
 
-var App = React.createClass({
-    getInitialState: function() {
-        return {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
             progress: 0.1,
             options: {}
         };
-    },
-
-    render: function() {
-        return <Circle initialAnimate={this.state.initialAnimate} options={this.state.options} progress={this.state.progress} />;
-    },
-
-    componentDidMount: function() {
-        var self = this;
-        setTimeout(function() {
-            console.log('setstate')
-            self.setState({
-                progress: 1
-            });
-        }, 1000);
-
-        setTimeout(function() {
-            console.log('setstate')
-            self.setState({
-                initialAnimate: true,
-                progress: 1
-            });
-        }, 500);
-
     }
-});
+
+    render() {
+        console.log("render");
+        return <Circle initialAnimate={this.state.initialAnimate} options={this.state.options} progress={this.state.progress} />;
+    }
+
+    componentDidMount() {
+        // var self = this;
+        // setTimeout(function() {
+        //     console.log('setstate')
+        //     self.setState({
+        //         progress: 1
+        //     });
+        // }, 1000);
+
+        // setTimeout(function() {
+        //     console.log('setstate')
+        //     self.setState({
+        //         initialAnimate: true,
+        //         progress: 1
+        //     });
+        // }, 500);
+    }
+}
 
 ReactDom.render(
     <App />,
-    document.querySelector('body')
+    document.querySelector('#progress')
 );
