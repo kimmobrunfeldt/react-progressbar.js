2ac7c51abf79ffb42fb39e66df0c4ac8cde7cc4a
Nathan Flurry <developer@nathanflurry.com>
2017-05-18 21:18:46 -0700

    Added type file

---
+++ typings/react-progressbar.js.d.ts
@@ -0,0 +1,60 @@
+declare module "react-progressbar.js" {
+    import React = require("react");
+
+    export interface ShapeOptions {
+        color?: string,
+        strokeWidth?: number,
+        trailColor?: string,
+        trailWidth?: number,
+        svgStyle?: {
+            display?: string,
+            width?: string
+        },
+        text?: {
+            value?: string,
+            className?: string,
+            style?: {
+                color?: string,
+                position?: string,
+                left?: string,
+                top?: string,
+                padding?: number,
+                margin?: number,
+                transform?: {
+                    prefix?: boolean,
+                    value?: string
+                }
+            },
+            autoStyleContainer?: boolean,
+            alignToBottom?: boolean
+        },
+        fill?: string,
+        duration?: number,
+        easing?: string,
+        from?: { color?: string },
+        to?: { color?: string },
+        step?: (state, circle, attachment) => void,
+        warnings?: boolean
+    }
+
+    export interface ShapeProps {
+        // ShapeClass: any;
+        options?: ShapeOptions;
+        progress?: number;
+        text?: string;
+        initialAnimate?: boolean;
+        containerStyle?: any;
+        containerClassName?: string;
+    }
+    export interface ShapeState {
+        shape: any;
+    }
+
+    export class Shape extends React.Component<ShapeProps, ShapeState> { }
+
+    export class Line extends Shape { }
+
+    export class Circle extends Shape { }
+
+    export class SemiCircle extends Shape { }
+}
